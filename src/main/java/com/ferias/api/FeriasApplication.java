3ada7756da9f59d02050956f48f9349f955310da
leandro <ledeanri>
2020-02-07 19:44:07 -0300

    Commit inicial

---
+++ src/main/java/com/ferias/api/FeriasApplication.java
@@ -0,0 +1,13 @@
+package com.ferias.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferias/api/FeriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferias.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
